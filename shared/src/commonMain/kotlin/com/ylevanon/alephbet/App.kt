529a1e37b4bbc85a568a640adcd1d91e0e578534
ylevanon <yoavlevanoni@outlook.com>
2026-08-23 14:29:47 -0700

    Remove temporary navigation demo

--- shared/src/commonMain/kotlin/com/ylevanon/alephbet/App.kt
+++ shared/src/commonMain/kotlin/com/ylevanon/alephbet/App.kt
@@ -1,36 +1,19 @@
 package com.ylevanon.alephbet
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.ylevanon.alephbet.alphabet.data.BundledAlphabetRepository
 import com.ylevanon.alephbet.alphabet.presentation.AlphabetRoute
 import com.ylevanon.alephbet.alphabet.presentation.AlphabetViewModel
 import com.ylevanon.alephbet.design.theme.AlephBetTheme
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data object AlphabetDemoDestination
-
-@Serializable
-internal data object DetailDemoDestination
 
 @Composable
 @Preview
 fun App() {
-    val navController = rememberNavController()
     val alphabetViewModel = viewModel {
         AlphabetViewModel(
             alphabetRepository = BundledAlphabetRepository(),
@@ -39,37 +22,11 @@ fun App() {
 
     AlephBetTheme {
         Scaffold { innerPadding ->
-            NavHost(
-                navController = navController,
-                startDestination = AlphabetDemoDestination,
+            AlphabetRoute(
                 modifier = Modifier.padding(innerPadding),
-            ) {
-                composable<AlphabetDemoDestination> {
-                    AlphabetRoute(
-                        viewModel = alphabetViewModel,
-                        onLetterClick = {
-                            navController.navigate(DetailDemoDestination)
-                        },
-                    )
-                }
-
-                composable<DetailDemoDestination> {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                    ) {
-                        Text(text = "Temporary detail destination")
-                        Button(
-                            onClick = {
-                                navController.popBackStack()
-                            },
-                        ) {
-                            Text(text = "Back to alphabet")
-                        }
-                    }
-                }
-            }
+                viewModel = alphabetViewModel,
+                onLetterClick = {},
+            )
         }
     }
 }
